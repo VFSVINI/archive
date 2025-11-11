d78a8a6feb4cec5f0fd36f5a8586960cd249f78d
Vinícius França <vfs2@cin.ufpe.br>
2025-11-11 20:15:36 -0300

    ManipuuÃlando Arquivos

--- src/main/java/POO/Main.java
+++ src/main/java/POO/Main.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args){
+        String[] texto = new String[3];
         try {
 
             InputStream is = new FileInputStream("cadastro.csv");
@@ -11,9 +12,32 @@ public class Main {
             BufferedReader br = new BufferedReader(isr);
 
             String linha = br.readLine();
+            linha = br.readLine(); // COMEÇA O PROGRAMA A PARTIR DA SEGUNDA LINHA.
+
+            String nome;
+            int idade;
+            String estadocivil;
+            String profissao;
+            Double salario;
+            Pessoa p;
+
+
             while (linha != null){
 
                 System.out.println(linha);
+                texto = linha.split(",");
+
+                nome = texto[0].trim();
+                idade = Integer.parseInt(texto[1].trim());
+                estadocivil = texto[2].trim();
+                profissao = texto[3].trim();
+                salario = Double.valueOf(texto[4].trim());
+
+                p = new Pessoa(nome, idade, estadocivil, profissao, salario);
+                p.apresentar();
+
+
+                System.out.println(texto[0]+texto[1]+texto[2]+texto[3]+texto[4]);
                 linha = br.readLine();
             }
 
